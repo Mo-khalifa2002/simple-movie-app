d866fea1deaaef2bbf0ce027047e4e33f6fc1ec3
Mohamed32002 <mohamedkhalifa32002@gmail.com>
2022-06-03 20:15:03 +0200

    error 1 fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,8 @@ import { useDispatch } from "react-redux";
 import { addMovies, fetchAsyncShows } from "../features/MovieReducers";
 import axios from "axios";
 import Hero from "./Hero";
-import { useState } from "react";
 
 const Home = () => {
-  const [query, setQuery] = useState("");
-
   const dispatch = useDispatch();
 
   const fetchApi = async () => {
@@ -27,7 +24,7 @@ const Home = () => {
   const fetchShowsApi = async () => {
     // &s => for the search word
     // &type => for the type of data "movie, series, episode etc..."
-    const response = await axios.get(`?apiKey=6453f1bc&s=${query}&type=series`);
+    const response = await axios.get(`?apiKey=6453f1bc&s=friends&type=series`);
 
     // const data = await response.json();
     console.log(response.data);
@@ -37,16 +34,11 @@ const Home = () => {
   useEffect(() => {
     fetchApi();
     fetchShowsApi();
-  }, [query]);
+  }, []);
 
   useEffect(() => {
     dispatch(fetchAsyncShows());
   }, []);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setQuery(e.target.value);
-  };
 
   return (
     <div>
@@ -54,15 +46,8 @@ const Home = () => {
         <div className="w-70 container text-center d-flex justify-content-center align-items-enter">
           <div className="d-flex justify-content-center align-items-center">
             <form class="example" action="action_page.php">
-              <input
-                onChange={(e) => setQuery(e.target)}
-                type="text"
-                placeholder="Search.."
-                name="search"
-              />
-              <button onSubmit={handleSubmit} type="submit">
-                Search
-              </button>
+              <input type="text" placeholder="Search.." name="search" />
+              <button type="submit">Search</button>
             </form>
           </div>
         </div>
